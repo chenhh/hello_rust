5181fa263d5286872b4cfdcb84d7cc2e70433f91
chenhh <chenhh@eva00>
2021-12-14 14:19:38 +0800

    new variable project

---
+++ variables/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // 不可變的變數
+    let x = 5;
+    println!("The value of x is: {}", x);
+    // error, modify immutable variable
+    //x = 6;
+    println!("The value of x is: {}", x);
+}
