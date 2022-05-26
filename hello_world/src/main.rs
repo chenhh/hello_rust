bd4e2f769b03d85bfccd84d43f9ae9285bdf436d
chenhh <chenhh@t480s>
2022-05-26 20:12:22 +0800

    add my lib

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,10 @@
+extern crate hellolib;
+
 fn main() {
-    println!("Hello, world!");
+    let x = "hello world x";
+    let y = x.to_string();
+    println!("Hello, world! {}, {}", x, y);
+    let stry: String =  y.chars().into_iter().take(8).collect();
+    println!("{}, string len: {}", stry ,y.len());
+    println!("calling hellolib name:{}", hellolib::myhello("chenhh"));
 }
---
+++ hellolib/src/lib.rs
@@ -0,0 +1,3 @@
+pub fn myhello(name: &str) -> String {
+    format!("hello name: {}", name)
+}
