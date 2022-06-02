5219e121d2da50c95643f85c886b92fdc30f64dc
chenhh <chenhh@eva00>
2022-06-02 14:21:26 +0800

    修改：     Cargo.toml

--- lp_farmer_problem/src/main.rs
+++ lp_farmer_problem/src/main.rs
@@ -1,6 +1,6 @@
 use std::time::{Duration, Instant};
 use std::error::Error;
-use good_lp::{constraint, default_solver, Solution, SolverModel, variables,  highs};
+use good_lp::{constraint, default_solver, Solution, SolverModel, variables,  minilp};
 
 fn farmer(acre: u32)-> Result<(), Box<dyn Error>>{
      let bounds = 0;
@@ -16,17 +16,12 @@ fn farmer(acre: u32)-> Result<(), Box<dyn Error>>{
     let solution = vars.minimise(
         &objective
     )
-<<<<<<< HEAD
-        .using(highs) // multiple solvers available
-        .with(constraint!(x[0] + x[1] + x[2] <= 500.))
-=======
         .using(minilp) // multiple solvers available
         .with(constraint!(x[0] + x[1] + x[2] <= acre))
->>>>>>> 530d6bf02e90ecb0d8b79909cd8c88e5cfa9cac9
-        .with(constraint!(2.5 *x[0] + y[0] - w[0] >= 200.))
-        .with(constraint!(3 * x[1] + y[1] - w[1] >= 240.))
-        .with(constraint!(w[2] + w[3] - 20.* x[2] <=0 ))
-        .with(constraint!(w[2] <= 6000.))
+        .with(constraint!(2.5 *x[0] + y[0] - w[0] >= 200))
+        .with(constraint!(3 * x[1] + y[1] - w[1] >= 240))
+        .with(constraint!(w[2] + w[3]  <=  20* x[2] ))
+        .with(constraint!(w[2] <= 6000 ))
         .solve()?;
 
 
@@ -46,7 +41,7 @@ fn farmer(acre: u32)-> Result<(), Box<dyn Error>>{
 
 fn main() {
     let now = Instant::now();
-    for idx in 1..=3000 {
+    for idx in 1..=10000 {
         farmer(500*idx);
     }
      println!("elapsed:{:.4} secs", now.elapsed().as_secs_f32());
