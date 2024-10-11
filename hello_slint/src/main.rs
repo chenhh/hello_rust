6c84ba7adb7cac1ea4bb8f2be744246d25f33816
chenhh <chenhh@eva01>
2024-10-11 13:42:40 +0800

    修改：     hello_slint/src/main.rs

--- hello_slint/src/main.rs
+++ hello_slint/src/main.rs
@@ -2,16 +2,10 @@
 // #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 // 引進自定義的widgets, .slint路徑在build.rs中自定
 slint::include_modules!();
-//
-// fn main() -> Result<(), slint::PlatformError> {
-//     // 此處的MainWindow widget是由slint中export的元件
-//     let main_window = MainWindow::new()?;
-//     main_window.run()
-// }
 
 fn main() {
     use slint::Model;
-
+    // 此處的MainWindow widget是由slint中export的元件
     let main_window = MainWindow::new().unwrap();
 
     // Fetch the tiles from the model
