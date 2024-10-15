81898fd1cb27468b008cbca92a58177df4c3470c
chenhh <chenhh@eva01>
2024-10-15 15:43:07 +0800

    add rfd example

--- hello_rfd/src/main.rs
+++ hello_rfd/src/main.rs
@@ -1,20 +1,23 @@
 use rfd::AsyncFileDialog;
 use rfd::FileDialog;
+use futures::executor::block_on;
 
 fn main() {
-    // let future = async {
-    //     // let file = AsyncFileDialog::new()
-    //     //     .add_filter("text", &["txt", "rs"])
-    //     //     .add_filter("rust", &["rs", "toml"])
-    //     //     .set_directory("/")
-    //     //     .pick_file()
-    //     //     .await;
-    //
-    //     let data = file.unwrap().read().await;
-    // };
-    let files = FileDialog::new()
-        .add_filter("text", &["txt", "rs"])
-        .add_filter("rust", &["rs", "toml"])
-        .set_directory("/")
-        .pick_file();
+    let future = async {
+        let file = AsyncFileDialog::new()
+            .add_filter("text", &["txt", "rs"])
+            .add_filter("rust", &["rs", "toml"])
+            .set_directory("/")
+            .pick_file()
+            .await;
+
+        let data = file.unwrap().read().await;
+    };
+    block_on(future);
+
+    // let files = FileDialog::new()
+    //     .add_filter("text", &["txt", "rs"])
+    //     .add_filter("rust", &["rs", "toml"])
+    //     .set_directory("/")
+    //     .pick_file();
 }
